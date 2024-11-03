f954b8521607aad0bf8d2e134c2d4c4a2b99f05d
Dennis Michailov <denis.michailov@gmail.com>
2024-11-03 08:48:03 +0200

    Initial

---
+++ src/lib/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let disabled = false;
+  export let onClick: () => void;
+  let props = { ...$$restProps }
+
+  const clickHandler = () => {
+    console.log(':: click');
+    onClick?.();
+  }
+</script>
+
+<button class={`Button ${props.class}`} {disabled} style={props.style} on:click={clickHandler}>
+  <slot/>
+</button>
+
+<style>
+  .Button {
+    position: relative;
+  }
+</style>
